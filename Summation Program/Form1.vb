66463168d02478c8840ef8744ab74a9e7c9811c7
Administrator <Administrator@PSPKM2826-2258>
2022-04-05 12:13:13 +0800

    Add project files.

---
+++ Summation Program/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim intsum As Integer
+
+        If (Val(TextBox1.Text) > 0) Then
+            intsum = 0
+            For x = 1 To Val(TextBox1.Text)
+                intsum = intsum = x
+            Next x
+            Label2.Text = "Submattion of " & TextBox1.Text & " is " & Str(intsum) & ","
+        Else
+            Label2.Text = "Enter a positive integer only"
+        End If
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+End Class
